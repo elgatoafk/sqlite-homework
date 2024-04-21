e513df6d6d1ac21b6062dacd3d9ca7fa32228102
Olena <155203553+elgatoafk@users.noreply.github.com>
2024-04-21 12:31:07 +0300

    Create database

---
+++ uni.sql
@@ -0,0 +1,57 @@
+-- Table: groups
+DROP TABLE IF EXISTS groups;
+CREATE TABLE groups (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    group_name CHAR(4) UNIQUE NOT NULL
+    
+);
+
+-- Table: students
+DROP TABLE IF EXISTS students;
+CREATE TABLE students (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    student VARCHAR(255) UNIQUE NOT NULL,
+    group_id CHAR(4),
+    FOREIGN KEY (group_id) REFERENCES groups(id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
+
+
+
+-- Table: teachers
+DROP TABLE IF EXISTS teachers;
+CREATE TABLE teachers (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    teacher VARCHAR(255) UNIQUE NOT NULL
+);
+
+
+-- Table: subjects
+DROP TABLE IF EXISTS subjects;
+CREATE TABLE subjects (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    subject VARCHAR(255) UNIQUE NOT NULL,
+    teacher_id INTEGER,
+    FOREIGN KEY (teacher_id) REFERENCES teachers(id)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE
+);
+
+
+-- Table: marks
+DROP TABLE IF EXISTS marks;
+CREATE TABLE marks(
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    student_id INTEGER,
+    subject_id INTEGER,
+    mark TINYINT UNSIGNED NOT NULL,
+    date_of DATE NOT NULL,
+    FOREIGN KEY (student_id) REFERENCES students (id)
+      ON DELETE CASCADE
+      ON UPDATE CASCADE
+    FOREIGN KEY (subject_id) REFERENCES subjects (id)
+      ON DELETE CASCADE
+      ON UPDATE CASCADE
+);
+
